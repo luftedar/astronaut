ef07def350fe5074b289801bb8dc3b13a388a3c5
luftedar <ugurorcun91@gmail.com>
2022-05-10 22:42:29 +0300

    Add JSX for History Component

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
 import EnglishText from '../components/EnglishText';
 import TurkishText from '../components/TurkishText';
+import History from '../components/History';
 
 function App() {
   return (
     <div className="App">
       <EnglishText />
       <TurkishText />
+      <History />
     </div>
   );
 }
