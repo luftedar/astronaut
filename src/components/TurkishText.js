a7a7e06133a181f75a6bf6c0c13a9ea5fad2420e
luftedar <ugurorcun91@gmail.com>
2022-05-10 22:39:36 +0300

    Add JSX for TurkishText Component

---
+++ src/components/TurkishText.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TurkishText = () => {
+  return (
+    <div>TurkishText</div>
+  )
+}
+
+export default TurkishText
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,9 +1,11 @@
 import EnglishText from '../components/EnglishText';
+import TurkishText from '../components/TurkishText';
 
 function App() {
   return (
     <div className="App">
       <EnglishText />
+      <TurkishText />
     </div>
   );
 }
