9bdd8a75de909ad19b6a0002a4c5ef73f736b784
luftedar <ugurorcun91@gmail.com>
2022-05-12 14:58:12 +0300

    Add Translation State for EnglishText Component

--- src/components/EnglishText.js
+++ src/components/EnglishText.js
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types';
+import { useEffect, useState } from 'react';
 
-const EnglishText = ({ text }) => (
+const EnglishText = ({ text }) => {
+  const [translationValue, setTranslationValue] = useState('');
+  useEffect(() => {
+    setTranslationValue(text)
+  }, [text])
+  return (
+  
   <>
-    <textarea value={text} />
+    <textarea
+    value={translationValue}
+    onChange={(e) => {
+      setTranslationValue(e.target.value)
+    }}
+    />
   </>
-);
+);}
 
 EnglishText.propTypes = {
   text: PropTypes.string.isRequired,
