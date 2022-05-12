ff328620343b8e7beb17098f2bb72c40e4f81c09
luftedar <ugurorcun91@gmail.com>
2022-05-12 13:00:31 +0300

    Create voiceRecognition Logic in App.js

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,11 +1,13 @@
+import useVoice from '../helpers/useVoice';
 import EnglishText from '../components/EnglishText';
 import TurkishText from '../components/TurkishText';
 import History from '../components/History';
 
 function App() {
+  const voiceRecognition = useVoice();
   return (
     <div className="App">
-      <EnglishText />
+      <EnglishText startVoiceRecognition={voiceRecognition.start()} />
       <TurkishText />
       <History />
     </div>
