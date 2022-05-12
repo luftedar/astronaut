448a243b9ee2529655d709eb57b761c454337a8a
luftedar <ugurorcun91@gmail.com>
2022-05-12 14:58:25 +0300

    Fix Linters

--- src/components/EnglishText.js
+++ src/components/EnglishText.js
@@ -4,19 +4,20 @@ import { useEffect, useState } from 'react';
 const EnglishText = ({ text }) => {
   const [translationValue, setTranslationValue] = useState('');
   useEffect(() => {
-    setTranslationValue(text)
-  }, [text])
+    setTranslationValue(text);
+  }, [text]);
   return (
-  
-  <>
-    <textarea
-    value={translationValue}
-    onChange={(e) => {
-      setTranslationValue(e.target.value)
-    }}
-    />
-  </>
-);}
+
+    <>
+      <textarea
+        value={translationValue}
+        onChange={(e) => {
+          setTranslationValue(e.target.value);
+        }}
+      />
+    </>
+  );
+};
 
 EnglishText.propTypes = {
   text: PropTypes.string.isRequired,
