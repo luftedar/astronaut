90cb5a98b6033e7ae3235ce1f2893fa96f8019c0
luftedar <ugurorcun91@gmail.com>
2022-05-12 12:10:33 +0300

    Fix Linter Errors

--- src/helpers/useVoice.js
+++ src/helpers/useVoice.js
@@ -1,11 +1,12 @@
 const useVoice = () => {
-  let recognition = new SpeechRecognition();
+  const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  const recognition = new SpeechRecognition();
   recognition.continuous = false;
   recognition.lang = 'en-US';
   recognition.interimResults = false;
   recognition.maxAlternatives = 1;
 
   return recognition;
-}
+};
 
 export default useVoice;
