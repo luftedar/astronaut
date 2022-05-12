b8f094848255ffb99bbcd1b456f6ffa0bb6a98ad
luftedar <ugurorcun91@gmail.com>
2022-05-12 15:32:59 +0300

    Add Body For postTranslationData

--- src/service/services.js
+++ src/service/services.js
@@ -3,7 +3,17 @@ import axios from "axios";
 const libreTranslateEndPoint = "https://libretranslate.com/translate"
 
 const postTranslationData = async (englishSentence) => {
-  const translationResult = await axios.post(libreTranslateEndPoint, englishSentence).then(response => response.translatedText);
+  const translationResult = await axios({
+    method: 'post',
+    url: libreTranslateEndPoint,
+    body: JSON.stringify({
+      q: `${englishSentence}`,
+      source: "en",
+      target: "tr",
+      format: "text"
+    }),
+    headers: {"Content-Type": "applcation/json"}
+  }).then((response) => {response.translatedText});
   return translationResult;
 };
 
