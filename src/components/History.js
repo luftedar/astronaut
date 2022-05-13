f4710dc58fb761482be7e4b97a4ff5893ff30afe
luftedar <ugurorcun91@gmail.com>
2022-05-13 14:51:28 +0300

    Create History

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import uuid from 'react-uuid';
 
-const History = () => (
-  <div>History</div>
-);
+const History = () => {
+  const translationHistory = useSelector((state) => state.translationReducer);
+  return (
+    <div>
+      <ul>
+        {translationHistory.map((history) => (
+          <li key={uuid()}>{history}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default History;
