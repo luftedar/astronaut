d7c9d87c362ab207c4f91df8fe6bb7b73bbf2a98
luftedar <ugurorcun91@gmail.com>
2022-05-14 11:01:23 +0300

    Fix TurkishText render issue

--- src/components/TurkishText.js
+++ src/components/TurkishText.js
@@ -3,7 +3,9 @@ import { useSelector } from 'react-redux';
 const TurkishText = () => {
   const translationResults = useSelector((state) => state.translationReducer);
   return (
-    <textarea disabled value={(translationResults[translationResults.length - 1])[1]} />
+    translationResults.length !== 0
+      ? <textarea disabled value={(translationResults[translationResults.length - 1])[1]} />
+      : <textarea disabled />
   );
 };
 
