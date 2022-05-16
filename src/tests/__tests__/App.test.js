446270f6776fdbe0a56e16b6a738cc72007844dc
luftedar <ugurorcun91@gmail.com>
2022-05-16 03:17:52 +0300

    Add Test For App

---
+++ src/tests/__tests__/App.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import {store} from '../../redux/configureStore'
+import App from '../../pages/App';
+
+describe('App page is working', () => {
+  test('Renders App', () => {
+    const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+    const tree = render(
+      <Provider store={store}>
+        {isChrome
+         &&
+         <App />
+        }
+      </Provider>
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
