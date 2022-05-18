4c70986704fbb3a44fa21fde5254da4f1607f169
luftedar <ugurorcun91@gmail.com>
2022-05-18 17:55:46 +0300

    Mock useDispatch

---
+++ src/tests/SelectLanguage.test.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { render } from '@testing-library/react';
+import { Provider, useDispatch } from "react-redux";
+import { store } from "../redux/configureStore";
+import LanguageSelect from '../components/LanguageSelect';
+import { changeInputLanguage, changeOutputLanguage } from '../redux/languagesStore';
+
+describe('LanguageSelect page is working', () => {
+  test('Renders LanguageSelect', () => {
+    const mockDispatch = jest.fn();
+    jest.mock('react-redux', () => ({
+      useDispatch: () => mockDispatch
+    }));
+    const tree = render(
+      <Provider store={store}>
+        <LanguageSelect
+        languages={[{
+          code: 'en',
+          name: 'English'
+        }]}
+        type='inputValue'
+        changeLanguage={(value) => {
+          if (value === 'inputValue') {
+            useDispatch.mockedDispatch(changeInputLanguage(value))
+          } else {
+            useDispatch.mockDispatch(changeOutputLanguage(value));
+          }
+        }}
+        />
+      </Provider>
+    )
+  })
+})
