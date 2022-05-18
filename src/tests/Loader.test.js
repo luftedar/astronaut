be8e393bdd457f59f9c0baa7812b80f56390642e
luftedar <ugurorcun91@gmail.com>
2022-05-18 17:35:44 +0300

    Test for Loading Component

---
+++ src/tests/Loader.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { store } from '../redux/configureStore'; 
+import Loader from '../components/Loader';
+
+describe('Loading page is working', () => {
+  test('Renders Loading', () => {
+    const tree = render(
+      <Provider store={store} >
+        <Loader />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
